9b1591c62abcb27ed585a88a89107346c8fe5832
Mig <r121221@yandex.ru>
2024-11-24 18:04:06 +0300

    proxy

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def main():
 
     # Загружаем конфиг в переменную config
     config: Config = load_config()
-    session = AiohttpSession(proxy="SOCKS5://5.183.70.46:1080")
+    session = AiohttpSession(proxy="http://proxy.server:3128")
     # Инициализируем бот и диспетчер
     bot = Bot(
         token=config.tg_bot.token,
